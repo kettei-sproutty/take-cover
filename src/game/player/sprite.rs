ae39c988fd4d239f1cac98710400b3eb090fd8b9
BackupMiles <leanwithmiles@gmail.com>
2024-07-29 01:30:00 +0200

    ref: load as asssets

---
+++ src/game/player/sprite.rs
@@ -0,0 +1,29 @@
+use bevy::prelude::*;
+
+use crate::{assets::UiAssets, game::common::animations::AnimationTimer};
+
+use super::{ANIMATION_RATE, PLAYER_DEFAULT_SPRITE_INDEX};
+
+pub fn get_main_animation(
+  mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
+  image_handle: Res<UiAssets>,
+) -> (Handle<Image>, TextureAtlas, AnimationTimer) {
+  let texture = image_handle.player_spritesheet.clone();
+  let layout = TextureAtlasLayout::from_grid(
+    UVec2::new(26, 22),
+    11,
+    11,
+    Some(UVec2::new(114, 23)),
+    Some(UVec2::new(47, 23)),
+  );
+  let texture_atlas_layouts = texture_atlas_layouts.add(layout);
+
+  (
+    texture,
+    TextureAtlas {
+      layout: texture_atlas_layouts,
+      index: PLAYER_DEFAULT_SPRITE_INDEX,
+    },
+    AnimationTimer(Timer::from_seconds(ANIMATION_RATE, TimerMode::Repeating)),
+  )
+}
